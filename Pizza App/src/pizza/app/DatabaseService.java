7a889e00a4d58e95a59807c01ac75abd8078d7cd
Feris Jumbo <cgartner37@gmail.com>
2022-12-10 15:11:26 -0600

    Finished GET and GETAsTable functions

--- Pizza App/src/pizza/app/DatabaseService.java
+++ Pizza App/src/pizza/app/DatabaseService.java
@@ -1,10 +1,10 @@
 package pizza.app;
 
-import java.sql.*;
+import java.util.ArrayList;
 
-/**
- * Connects to a database and is to be used statically.
- */
+import java.sql.*;
+import io.bretty.console.table.*;
+
 public class DatabaseService {
     private String _URL,
             _user,
@@ -13,7 +13,8 @@ public class DatabaseService {
     private static Connection _connection;
     
     /**
-     * Connects to the database and stores its connection statically.
+     * Setups up the connection to the database with the provided information.
+     * 
      * @param host
      * @param port
      * @param db
@@ -31,20 +32,75 @@ public class DatabaseService {
     }
     
     /**
-     * Handles GET requests from the database.
+     * Will GET information from the database.
+     * 
      * @param query
-     * @throws SQLException 
+     * @return 
      */
     public static ResultSet GET(String query) {
         try {
             Statement statement = _connection.createStatement();
             ResultSet resultSet = statement.executeQuery(query);
 
-            
             return resultSet;
         } catch (SQLException e) { e.printStackTrace(); }
         
         return null;
+    }
+    
+    /**
+     * Returns a Table object which contains the query and can be directly
+     * printed to screen.
+     * @param headers
+     * @param query
+     * @return 
+     */
+    public static Table GETAsPrintableTable(String query) {
+        ColumnFormatter<String> cf = ColumnFormatter.text(Alignment.CENTER, 15);
+        
+        ResultSet rs = GET(query);
+        
+        ArrayList<ArrayList<String>> data = new ArrayList<ArrayList<String>>();
+        
+        try {
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnCount = rsmd.getColumnCount();
+            
+            String[] headers = new String[columnCount];
+            for (int i = 1; i <= columnCount; i++) {
+                headers[i-1] = rsmd.getColumnName(i).toUpperCase();
+            }
+            
+            while (rs.next()) {
+                ArrayList<String> row = new ArrayList<String>();
+                for (int i = 0; i < columnCount; i++) {
+                    row.add(rs.getString(i+1));
+                }
+                data.add(row);
+            }
+            
+            // Convert Multidimensional ArrayList to Array
+            String[][] dataAsArray = new String[data.size()][];
+            for (int i = 0; i < data.size(); i++) {
+                ArrayList<String> row = data.get(i);
+                dataAsArray[i] = row.toArray(new String[row.size()]);
+            }
+            
+            return Table.of(headers, dataAsArray, cf);
+            
+        } catch (SQLException e) { e.printStackTrace(); }
+        
+        return null;
+    }
+    
+    /**
+     * Posts the query to the database.  Used for INSERT, UPDATE, and DROP
+     * queries.
+     * @param query
+     * @return 
+     */
+    public static boolean POST(String query) {
+        return false;
     }
     
 }
--- Pizza App/src/pizza/app/Program.java
+++ Pizza App/src/pizza/app/Program.java
@@ -1,7 +1,7 @@
 package pizza.app;
 
 import java.sql.*;
-import io.bretty.console.table.*;
+import io.bretty.console.table.Table;
 
 /**
  * Main method and starting point of program.
@@ -9,19 +9,11 @@ import io.bretty.console.table.*;
 public class Program {
     
     // Main Method
-    public static void main(String[] args) throws SQLException {
-        // Establish connection to Database
-        new DatabaseService("localhost", "5432", "finalproject", "postgres", "admin");
+    public static void main(String[] args) {
+        new DatabaseService("localhost", "5432", "postgres", "postgres", "root");
         
-       ResultSet rs = DatabaseService.GET("SELECT * FROM toppings;");
-    
-       rs.next();
-       
-       String[] headers = {"Crust Type"};
-       String[][] data = { {rs.getString(1)} };
-       ColumnFormatter<String> cf = ColumnFormatter.text(Alignment.CENTER, 16);
-       
-       Table table = Table.of(headers, data, cf);
-       System.out.println(table);
+        Table query = DatabaseService.GETAsPrintableTable("SELECT employeeid as id, title FROM employee");
+        
+        System.out.println(query);
     }
 }
