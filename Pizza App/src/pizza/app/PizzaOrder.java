e137a98648d20c89303f0d2e0f25e247aae74760
SomeGuy143 <106126762+SomeGuy143@users.noreply.github.com>
2022-12-10 22:09:29 -0600

    Add files via upload

---
+++ Pizza App/src/pizza/app/PizzaOrder.java
@@ -0,0 +1,28 @@
+package pizza.app;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * Class to hold the user's selected options for their pizza order
+ */
+public class PizzaOrder {
+    
+    private String crust;
+    private String cheese;
+    private ArrayList<String> toppings;
+    private int quantity;
+    
+    
+   public String getCrust() { return crust; }
+   public String getCheese() { return cheese; }
+   public ArrayList<String> getToppings() { return toppings; }
+   public int getQuantity() { return quantity; }
+   
+   
+   public void setCrust(String crust) { this.crust = crust; }
+   public void setCheese(String cheese) { this.cheese = cheese; }
+   public void setToppings(String topping) { this.toppings.add(topping); }
+   public void setQuantity(int quantity) { this.quantity = quantity; }
+    
+}
